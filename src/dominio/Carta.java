8c74ebadff616f85515c1995330cd8be255c3488
Lukas Cortes <mrvro@NotebookToshi>
2026-06-14 20:35:03 -0400

    Clase abstracta para Carta

---
+++ src/dominio/Carta.java
@@ -0,0 +1,30 @@
+package dominio;
+
+public abstract class Carta {
+	protected String nombre;
+	protected int rareza;
+	protected String tipo;
+	public Carta(String nombre, int rareza, String tipo) {
+		this.nombre = nombre;
+		this.rareza = rareza;
+		this.tipo = tipo;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getRareza() {
+		return rareza;
+	}
+	public void setRareza(int rareza) {
+		this.rareza = rareza;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+}
