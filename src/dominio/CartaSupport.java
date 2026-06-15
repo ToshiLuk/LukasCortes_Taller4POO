c07f0d2d53d9bec985bb0e48661431533bf3ff9d
Lukas Cortes <mrvro@NotebookToshi>
2026-06-14 20:47:30 -0400

    CartaSupport

--- src/dominio/CartaSupport.java
+++ src/dominio/CartaSupport.java
@@ -1,5 +1,24 @@
 package dominio;
 
-public class CartaSupport {
+public class CartaSupport extends Carta{
+	private int efectoPorTurno;
 
+	public CartaSupport(String nombre, int rareza, String tipo, int efectoPorTurno) {
+		super(nombre, rareza, tipo);
+		this.efectoPorTurno = efectoPorTurno;
+	}
+
+	public int getEfectoPorTurno() {
+		return efectoPorTurno;
+	}
+
+	public void setEfectoPorTurno(int efectoPorTurno) {
+		this.efectoPorTurno = efectoPorTurno;
+	}
+
+	@Override
+	public double aceptar(Visitor visitor) {
+		return visitor.visitar(this);
+	}
+	
 }
