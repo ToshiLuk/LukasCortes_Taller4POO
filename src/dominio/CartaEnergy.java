7a63716b6fe0c293be30d4d454eb7da4e6930f48
Lukas Cortes <mrvro@NotebookToshi>
2026-06-14 20:48:45 -0400

    CartaEnergy

--- src/dominio/CartaEnergy.java
+++ src/dominio/CartaEnergy.java
@@ -1,5 +1,24 @@
 package dominio;
 
-public class CartaEnergy {
+public class CartaEnergy extends Carta{
+	private String elemento;
 
+	public CartaEnergy(String nombre, int rareza, String tipo, String elemento) {
+		super(nombre, rareza, tipo);
+		this.elemento = elemento;
+	}
+
+	public String getElemento() {
+		return elemento;
+	}
+
+	public void setElemento(String elemento) {
+		this.elemento = elemento;
+	}
+
+	@Override
+	public double aceptar(Visitor visitor) {
+		return visitor.visitar(this);
+	}
+	
 }
