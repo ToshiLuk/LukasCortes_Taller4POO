89429732f1f03c4db8107e7fabfefe805eb57359
Lukas Cortes <mrvro@NotebookToshi>
2026-06-14 20:46:15 -0400

    CartaItem metodos, getter, setter, constructor y visitor

--- src/dominio/CartaItem.java
+++ src/dominio/CartaItem.java
@@ -1,5 +1,24 @@
 package dominio;
 
-public class CartaItem {
+public class CartaItem extends Carta{
+	private int bonificacion;
 
+	public CartaItem(String nombre, int rareza, String tipo, int bonificacion) {
+		super(nombre, rareza, tipo);
+		this.bonificacion = bonificacion;
+	}
+	
+	public int getBonificacion() {
+		return bonificacion;
+	}
+
+	public void setBonificacion(int bonificacion) {
+		this.bonificacion = bonificacion;
+	}
+
+	@Override
+	public double aceptar(Visitor visitor) {
+		return visitor.visitar(this);
+	}
+	
 }
