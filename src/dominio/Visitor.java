8860d263e52926259ddf7371a0fa59bbc6f6af0b
Lukas Cortés <lukas.cortes01@alumnos.ucn.cl>
2026-06-17 09:16:49 -0400

    Delete src/dominio/Visitor.java

--- src/dominio/Visitor.java
+++
@@ -1,8 +0,0 @@
-package dominio;
-
-public interface Visitor {
-	double visitar(CartaPokemon pokemon);
-	double visitar(CartaItem item);
-	double visitar(CartaSupport supp);
-	double visitar(CartaEnergy energy);
-}
