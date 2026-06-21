c7e8a6cfb4cf9e52a9b95c6228c85263e26eb7c3
Lukas Cortes <mrvro@NotebookToshi>
2026-06-21 18:14:52 -0400

    Mas GUI

--- src/GUI/VentanaPrincipal.java
+++ src/GUI/VentanaPrincipal.java
@@ -6,24 +6,33 @@ import logica.SistemaImp;
 public class VentanaPrincipal extends JFrame{
 	private Sistema sistema;
 	public VentanaPrincipal() {
+		//Patron Singleton
 		sistema = SistemaImp.getInstancia();
 		sistema.guardarCartas();
-		
+		//Ventana
 		setTitle("Gestor de Coleccion - Pokemon TCG");
 		setSize(800,600);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
-		
+		//Contenedor de pestañas
 		JTabbedPane pestañas = new JTabbedPane();
+		//Paneles
 		JPanel panelAdmin = crearPanelAdmin();
 		JPanel panelColeccion = crearPanelColeccion();
+		//Y se agregan los paneles a sus pestañas 
+		pestañas.addTab("Admin", panelAdmin);
+		pestañas.addTab("Ver Coleccion", panelColeccion);
+		
+		add(pestañas);
 	}
 	private JPanel crearPanelAdmin() {
-		// TODO Auto-generated method stub
-		return null;
+		JPanel panel = new JPanel();
+		panel.add(new JLabel("PlaceholderAdmin"));
+		return panel;
 	}
 	private JPanel crearPanelColeccion() {
-		// TODO Auto-generated method stub
-		return null;
+		JPanel panel = new JPanel();
+		panel.add(new JLabel("PlaceHolderColeccion"));
+		return panel;
 	}
 }
